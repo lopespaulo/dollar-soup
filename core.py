0662245e69e33684857f755c87d7caa1b5a91049
Paulo Lopes <paulo.hq.lopes@gmail.com>
2015-05-12 14:37:13 -0300

    Cleanning data

--- core.py
+++ core.py
@@ -17,16 +17,34 @@ def get_html(url):
 
 #escovando dados
 def clean_data(dados):
-    only_table_tags = SoupStrainer("td")
+    only_table_tags = SoupStrainer("td") #Define a Tag parseada
     soup = BeautifulSoup(dados, 'html.parser', parse_only=only_table_tags)
     soup = soup.get_text('|', strip=True)
-    valor = [valor for valor in soup.split('|')]
+    valor = [valor.replace(",", ".") for valor in soup.split('|')]
     date = datetime.strptime(valor[0], "%d/%m/%Y")
     valor[0] = date.strftime("%Y/%m/%d")
-    try:
-        cursor.execute("INSERT INTO dollar (dollar_dia, dollar_compra, dollar_venda) VALUES (%s, %s, %s)",(str(valor[0]), str(valor[1]), str(valor[2])))
-    except mariadb.Error as error:
-        print("Error: {}".format(error))
+
+    #A trabalhar ######
+    # try:
+    #     cursor.execute("""
+    #         INSERT INTO dollar (dollar_dia, dollar_compra, dollar_venda)
+    #         VALUES (%s, %s, %s)
+    #         ON DUPLICATE KEY UPDATE dollar_dia=%s",(str(valor[0]), str(valor[1]), str(valor[2]), str(valor[0]))""")
+    # except mariadb.Error as error:
+    #     print("Error: {}".format(error))
+    # mariadb_connection.commit()
+
+#Otimizar as consultas!
+    if cursor.execute("SELECT * FROM dollar WHERE dollar_dia = %s", (valor[0])) < 1:
+        try:
+            cursor.execute("INSERT INTO dollar (dollar_dia, dollar_compra, dollar_venda) VALUES (%s, %s, %s)",(str(valor[0]), str(valor[1]), str(valor[2])))
+        except mariadb.Error as error:
+            print("Error: {}".format(error))
+    else:
+        try:
+            cursor.execute("UPDATE dollar SET dollar_compra = %s, dollar_venda=%s WHERE dollar_dia = %s",(str(valor[1]), str(valor[2]), str(valor[0])))
+        except mariadb.Error as error:
+            print("Error: {}".format(error))
     mariadb_connection.commit()
 
 url = 'https://www3.bcb.gov.br/ptax_internet/consultarUltimaCotacaoDolar.do'
